3e897e911f011027d33dd15c97375f44e686d6c8
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2023-11-22 22:28:13 +0800

    chore: init

---
+++ source/utilities/storage_helpers.ts
@@ -0,0 +1,39 @@
+import browserAPI from "browser";
+
+const { storage } = browserAPI;
+const KEY = "storage_key";
+
+interface Storage {
+  storage_key: string;
+}
+
+interface Update {
+  storage_key?: {
+    newValue?: string;
+    oldValue?: string;
+  };
+}
+
+/**
+ * In practice, I'd usually just export these as a hook for simplicity.
+ * Keeping it as separate utility just to demonstrate unit testing with mock.
+ * Check out storage_helpers.test.ts
+ */
+
+// Add listener to update on any changes to browser-sync storage
+export function addStorageListener(
+  callback: (str: string) => void,
+): () => void {
+  const updateStorage = (data: Update) => callback(data[KEY]?.newValue || "");
+  storage.onChanged.addListener(updateStorage);
+  return () => storage.onChanged.removeListener(updateStorage);
+}
+
+export async function getStorage() {
+  const data = await storage.sync.get(KEY);
+  return data[KEY];
+}
+
+export function updateStorage(inputData: string) {
+  storage.sync.set({ [KEY]: inputData });
+}
