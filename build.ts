1cf1b6024124e177255ee461efe969eaeec60c5c
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2023-12-11 11:59:24 +0800

    chore: use import keyword instead

--- build.ts
+++ build.ts
@@ -84,12 +84,16 @@ const builds = Object.keys(browsers).map(async (browserId) => {
   };
 
   // Build Deno Plugin Options
-  let importMapURL = new URL("file://" + resolve("./import_map.json"));
+  let importMapURL = new URL("file://" + resolve("./import_map.json"))
+    .toString();
   if (!existsSync(importMapURL)) {
     const denoJSONFileURL = new URL("file://" + resolve("./deno.json"));
     const denoJSON = await (await fetch(denoJSONFileURL)).json();
     if (denoJSON.source || denoJSON.imports) {
-      importMapURL = denoJSONFileURL;
+      const importMap = {
+        imports: denoJSON.imports,
+      };
+      importMapURL = `data:application/json,${JSON.stringify(importMap)}`;
     }
   }
 
